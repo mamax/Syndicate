7b9d544c1f4280903b0075010285fddaa15fd5fd
Maksym Mazurkevych <maksym_mazurkevych@epam.com>
2024-01-26 16:00:07 +0200

    Removed alias_name from HelloWorld

--- task02/jsrc/main/java/com/task02/HelloWorld.java
+++ task02/jsrc/main/java/com/task02/HelloWorld.java
@@ -16,8 +16,7 @@ import java.util.Map;
 
 @LambdaHandler(lambdaName = "hello_world",
 	roleName = "hello_world-role",
-	isPublishVersion = true,
-	aliasName = "${lambdas_alias_name}"
+	isPublishVersion = true
 )
 
 @LambdaUrlConfig(
