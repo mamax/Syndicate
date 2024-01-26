53f6e7896a19f1eef92549d589b250d5da006f29
Maksym Mazurkevych <maksym_mazurkevych@epam.com>
2024-01-26 16:13:42 +0200

    Revert "[revert] - Added new RequestHandler for configuring lambda function"
    This reverts commit 3c02a82a32c19ba7b3bb3d622291a06ef9535738.

--- task02/jsrc/main/java/com/task02/HelloWorld.java
+++ task02/jsrc/main/java/com/task02/HelloWorld.java
@@ -2,8 +2,6 @@ package com.task02;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
-import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
-import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.syndicate.deployment.annotations.LambdaUrlConfig;
@@ -23,26 +21,17 @@ import java.util.Map;
 		authType = AuthType.NONE,
 		invokeMode = InvokeMode.BUFFERED
 )
-public class HelloWorld implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
-	private static final int SC_OK = 200;
-	private static final int SC_BAD_REQUEST = 400;
+public class HelloWorld implements RequestHandler<Map<String,Object>, String> {
 	Gson gson = new GsonBuilder().setPrettyPrinting().create();
 
 	@Override
-	public APIGatewayProxyResponseEvent handleRequest(APIGatewayProxyRequestEvent apiGatewayProxyRequestEvent, Context context) {
-		Map<String, Object> resultMap = new HashMap<String, Object>();
-		resultMap.put("statusCode", 200);
-		resultMap.put("body", "Hello from Lambda");
-		gson.toJson(resultMap);
-		context.getLogger().log(apiGatewayProxyRequestEvent.toString());
-		try {
-			return new APIGatewayProxyResponseEvent()
-					.withStatusCode(SC_OK)
-					.withBody(gson.toJson(resultMap));
-		} catch (IllegalArgumentException exception) {
-			return new APIGatewayProxyResponseEvent()
-					.withStatusCode(SC_BAD_REQUEST)
-					.withBody(gson.toJson("An error message appeared"));
-		}
+	public String handleRequest(Map<String, Object> event, Context context) {
+		//create a map to store your results
+		Map<String, Object> responseMap = new HashMap<>();
+		responseMap.put("statusCode", 200);
+		responseMap.put("message", "Hello from Lambda");
+		String jsonResult = gson.toJson(responseMap);
+		//return a json string
+		return jsonResult;
 	}
 }
